f241b4193d676c025cea88371e5c9b2cb264a318
Ali dauda <alidauda14@gmail.com>
2025-07-16 21:56:09 +0100

    Initial commit with project setup and basic structure.

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+import uvicorn
+from solders.keypair import Keypair
+
+import base58
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from backend!"}
+
+
+@app.get("/generate_wallet")
+def generate_wallet():
+    keypair = Keypair()
+    private_key_bytes = bytes(keypair)
+    private_key_str = base58.b58encode(private_key_bytes).decode("utf-8")
+    return {
+        "public_key": str(keypair.pubkey()),
+        "private_key": private_key_str,
+    }
+
+@app.get("/rug_check")
+def rug_check(public_key: str, private_key: str):
+    keypair = Keypair.from_base58_string(private_key)  # type: ignore
+    public_key = str(keypair.pubkey())
+    
+    
+    return {"message": "Rug check!", }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
